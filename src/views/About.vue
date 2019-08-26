5a119d8bf47232babf785ec1226a50fac501f360
Edison Un <chonwaiun@gmail.com>
2019-08-26 21:10:39 +0800

    Modify the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-container flex justify-center items-center p-4 flex-col h-screen w-screen">
+    <h1>This project only for Mobile Web 2019</h1>
+    <section class="mt-4">
+      <div>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/gregor-cresnar"
+          title="Gregor Cresnar"
+        >Gregor Cresnar</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
