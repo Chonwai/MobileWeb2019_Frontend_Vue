ac2f09a5b995fcf00b44b0bc5fade2614a465331
Edison Un <chonwaiun@gmail.com>
2019-09-09 16:23:03 +0800

    Add the vue-svg plugin into the project

--- src/components/FooterNavigation/FooterNavigationIndex.vue
+++ src/components/FooterNavigation/FooterNavigationIndex.vue
@@ -1,15 +1,35 @@
 <template>
-    <nav class="footer-nav flex items-center h-12 bg-blue-500">
-        <button class="h-full w-12"></button>
-    </nav>
+  <nav class="footer-nav flex items-center h-12 bg-blue-500">
+    <button class="flex-1 h-10 w-10 text-white flex justify-center items-center">
+      <img class="w-auto h-full" src="@/assets/img/shop.svg"/>
+      <!-- <shopIcon class="w-auto h-full"/> -->
+    </button>
+    <button class="flex-1 h-10 w-10 text-white flex justify-center items-center">
+      <img class="w-auto h-full" src="@/assets/img/shop.svg" />
+    </button>
+    <button class="flex-1 h-10 w-10 text-white flex justify-center items-center">
+      <p class="h-10 w-10 border-2 border-white rounded-full text-white text-2xl font-bold">+</p>
+    </button>
+    <button class="flex-1 h-10 w-10 text-white flex justify-center items-center">
+      <img class="w-auto h-full" src="@/assets/img/shop.svg" />
+    </button>
+    <button class="flex-1 h-10 w-10 text-white flex justify-center items-center">
+      <img class="w-auto h-full" src="@/assets/img/shop.svg" />
+    </button>
+  </nav>
 </template>
 
 <script>
+import shopIcon from '@/assets/img/shop.svg';
 export default {
-    
-}
+    components: {
+        shopIcon
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-    
+    img {
+        fill: white;
+    }
 </style>
