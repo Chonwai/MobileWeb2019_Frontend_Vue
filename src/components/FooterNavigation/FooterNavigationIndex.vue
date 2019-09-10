af61187aa5141744ed3c578e94ea0c2c96576809
Edison Un <chonwaiun@gmail.com>
2019-09-10 12:46:44 +0800

    Change the icon from png to svg

--- src/components/FooterNavigation/FooterNavigationIndex.vue
+++ src/components/FooterNavigation/FooterNavigationIndex.vue
@@ -1,38 +1,65 @@
 <template>
   <nav class="footer-nav flex items-center h-14 bg-blue-500">
-    <router-link class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col" :to="{path: '/'}">
-      <img class="w-8 h-8" src="@/assets/img/home.svg"/>
-      <p class="text-xs">首頁</p>
+    <router-link
+      class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col"
+      :to="{path: '/'}"
+    >
+      <homeIcon class="w-6 h-6" fill="white" />
+      <!-- <p class="text-xs">首頁</p> -->
     </router-link>
-    <router-link class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col" :to="{path: '/forum'}">
-      <img class="w-8 h-8" src="@/assets/img/chat.svg"/>
-      <p class="text-xs">討論區</p>
+    <router-link
+      class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col"
+      :to="{path: '/forum'}"
+    >
+      <chatIcon class="w-6 h-6" fill="white" />
+      <!-- <p class="text-xs">討論區</p> -->
     </router-link>
-    <router-link class="flex-1 h-12 w-12 text-white flex justify-center items-center" :to="{path: 'new'}">
-      <p class="h-12 w-12 border-2 border-white rounded-full text-white text-2xl font-bold flex justify-center items-center">+</p>
+    <router-link
+      class="flex-1 h-12 w-12 text-white flex justify-center items-center"
+      :to="{path: 'new'}"
+    >
+        <addIcon class="w-6 h-6" fill="white" />
+      <!-- <p
+        class="h-12 w-12 border-2 border-white rounded-full text-white text-2xl font-bold flex justify-center items-center"
+      >+</p> -->
     </router-link>
-    <router-link class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col" :to="{path: '/shop'}">
-      <img class="w-8 h-8" src="@/assets/img/placeholder.svg"/>
-      <p class="text-xs">商戶列表</p>
+    <router-link
+      class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col"
+      :to="{path: '/shop'}"
+    >
+        <placeholderIcon class="w-6 h-6" fill="white" />
+      <!-- <p class="text-xs">商戶列表</p> -->
     </router-link>
-    <router-link class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col" :to="{path: '/about'}">
-      <img class="w-8 h-8" src="@/assets/img/info.svg"/>
-      <p class="text-xs">關於我們</p>
+    <router-link
+      class="flex-1 h-12 w-12 text-white flex justify-center items-center flex-col"
+      :to="{path: '/about'}"
+    >
+        <infoIcon class="w-6 h-6" fill="white" />
+      <!-- <p class="text-xs">關於我們</p> -->
     </router-link>
   </nav>
 </template>
 
 <script>
-import shopIcon from '@/assets/img/shop.svg';
+import homeIcon from "@/assets/img/home.svg?inline";
+import chatIcon from "@/assets/img/chat.svg?inline";
+import addIcon from "@/assets/img/pencil.svg?inline";
+import placeholderIcon from "@/assets/img/placeholder.svg?inline";
+import infoIcon from "@/assets/img/info.svg?inline";
+
 export default {
-    components: {
-        shopIcon
-    }
+  components: {
+    homeIcon,
+    chatIcon,
+    addIcon,
+    placeholderIcon,
+    infoIcon
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-    img {
-        fill: white;
-    }
+img {
+  fill: white;
+}
 </style>
--- src/views/Post/PostIndex.vue
+++ src/views/Post/PostIndex.vue
@@ -1,18 +1,34 @@
 <template>
-    <section class="post-component">
-        <div class="input-box">
+    <section class="post-component w-screen p-8">
+        <p class="w-full flex text-2xl font-bold">發表貼文</p>
+        <div class="input-box w-full flex mt-4">
+            <label for="">照片：</label>
+            <input class="border rounded-sm flex-1 p-2 truncate" type="file" accept="image/*" capture="camera">
+        </div>
+        <div class="input-box w-full flex mt-4">
             <label for="">標題：</label>
-            <input type="text" name="" id="">
+            <input class="border rounded-sm flex-1 p-2" type="text" name="" id="">
+        </div>
+        <div class="input-box w-full flex mt-4">
+            <label for="">內容：</label>
+            <textarea class="border rounded-sm flex-1 p-2" name="" id="" cols="10" rows="10"></textarea>
         </div>
     </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            title: '',
+            content: ''
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-    
+    textarea {
+        resize: none;
+    }
 </style>
